991ddbffc93198ee6a6141bb883af2b9351ccd13
Adrian Edwards <adrian@adriancedwards.com>
2024-02-07 15:45:07 -0500

    utilize discovery of service response data element name when handling SOAP responses

--- lib/dart_soap.dart
+++ lib/dart_soap.dart
@@ -38,11 +38,17 @@ class SoapClient {
       body: soapEnvelope,
     );
 
-    if (response.statusCode == 200) {
-      return _parseSoapResponse(response.body);
-    } else {
+    if (response.statusCode != 200) {
       throw Exception(
           'Failed to call the web service. Status code: ${response.statusCode}');
+    }
+
+    try {
+      return _parseSoapResponse(response.body, methodName)
+          .then((value) => value.value!);
+    } catch (e) {
+      throw Exception(
+          'Failed to parse the response from the web service. Error: $e');
     }
   }
 
@@ -70,11 +76,17 @@ class SoapClient {
     return envelope.buildDocument().toXmlString(pretty: true);
   }
 
-  String _parseSoapResponse(String response) {
+  Future<xml.XmlElement> _parseSoapResponse(
+      String response, String methodName) async {
     // Customize this method to parse the SOAP response.
     // This is a simplified example.
-    final document = xml.XmlDocument.parse(response);
-    final result = document.findAllElements('YourResultElementName').first.text;
-    return result;
+    final response_document = xml.XmlDocument.parse(response);
+    final response_element_name =
+        await wsdl.parseResponseDataElement(methodName);
+    final dataElement = response_document
+        .findAllElements(response_element_name) //, namespace: namespaces['web']
+        .first;
+
+    return dataElement;
   }
 }
