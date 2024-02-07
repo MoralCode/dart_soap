87033634f8add357f15070ae6c861894878176bd
Adrian Edwards <adrian@adriancedwards.com>
2024-02-07 17:27:38 -0500

    wsdldocument class from chatgpt

---
+++ lib/src/wsdl_document.dart
@@ -0,0 +1,86 @@
+import 'package:xml/xml.dart' as xml;
+
+class WsdlDocument {
+  final String serviceName;
+  final List<WsdlOperation> operations;
+  final List<WsdlType> types;
+
+  WsdlDocument(
+      {required this.serviceName,
+      required this.operations,
+      required this.types});
+
+  factory WsdlDocument.fromXml(String xmlContent) {
+    final document = xml.XmlDocument.parse(xmlContent);
+
+    final serviceName =
+        document.findAllElements('service').first.getAttribute('name');
+
+    final operations = document
+        .findAllElements('operation')
+        .map((operationElement) =>
+            WsdlOperation.fromXmlElement(operationElement))
+        .toList();
+
+    final types = document
+        .findAllElements('types')
+        .map((typesElement) => WsdlType.fromXmlElement(typesElement))
+        .toList();
+
+    return WsdlDocument(
+        serviceName: serviceName, operations: operations, types: types);
+  }
+}
+
+class WsdlOperation {
+  final String name;
+  final String inputMessage;
+  final String outputMessage;
+
+  WsdlOperation(
+      {required this.name,
+      required this.inputMessage,
+      required this.outputMessage});
+
+  factory WsdlOperation.fromXmlElement(xml.XmlElement element) {
+    final name = element.getAttribute('name');
+    final inputMessage =
+        element.findElements('input').first.getAttribute('message');
+    final outputMessage =
+        element.findElements('output').first.getAttribute('message');
+
+    return WsdlOperation(
+        name: name, inputMessage: inputMessage, outputMessage: outputMessage);
+  }
+}
+
+class WsdlType {
+  final String name;
+  final List<WsdlElement> elements;
+
+  WsdlType({required this.name, required this.elements});
+
+  factory WsdlType.fromXmlElement(xml.XmlElement element) {
+    final name = element.getAttribute('name');
+    final elements = element
+        .findAllElements('element')
+        .map((elementElement) => WsdlElement.fromXmlElement(elementElement))
+        .toList();
+
+    return WsdlType(name: name, elements: elements);
+  }
+}
+
+class WsdlElement {
+  final String name;
+  final String type;
+
+  WsdlElement({required this.name, required this.type});
+
+  factory WsdlElement.fromXmlElement(xml.XmlElement element) {
+    final name = element.getAttribute('name');
+    final type = element.getAttribute('type');
+
+    return WsdlElement(name: name, type: type);
+  }
+}
